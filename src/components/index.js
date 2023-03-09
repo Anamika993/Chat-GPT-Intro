14c6382b0ed002872f2afa536d51dfd09a1aa594
Anamika993 <anamika.varshney_cs20@gla.ac.in>
2023-03-09 21:08:23 +0530

    commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './Article/Article';
+export {default as Brand} from './brand/brand';
+export {default as Feature} from './feature/feature';
+export {default as CTA} from './cta/cta';
+export {default as Navbar} from './navbar/Navbar';
